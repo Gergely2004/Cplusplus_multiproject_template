01911b221b3ba4a8a8808b5745c8c55c7db8b202
Gergely2004 <kis.tokai.gergely@student.ms.sapientia.ro>
2024-02-22 14:15:52 +0200

    int lnko tol minimiseselection-ig

--- lab-01/Headers/functions.h
+++ lab-01/Headers/functions.h
@@ -30,7 +30,7 @@ int lkktRec(int a, int b, int a1, int b1);
 int powerRec(int b, int e);
 void readArrayRec(int *a, int n, ifstream &f);
 void printArrayRec(int *a, int n);
-void printArrayRec(int *a, int n);
+void printArrayRecu(int *a, int n);
 int sumArrayRec(int *a, int n);
 int maxArrayRec(int *a, int n, int max);
 int lnkoArrayRec(int *a, int n);
