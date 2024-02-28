cfe6549d2d624392e9f0da176c11d95bb594c0cb
Gergely2004 <kis.tokai.gergely@student.ms.sapientia.ro>
2024-02-28 21:59:01 +0200

    lab 1 teljes megoldasa

--- lab-01/Sources/functions.cpp
+++ lab-01/Sources/functions.cpp
@@ -2,18 +2,19 @@
 #include "functions.h"
 
 int lnko(int a, int b) {
-    int m;
-    while(a != 0) {
-        m = a % b;
+    int r;
+    r = a % b;
+    while (r != 0) {
         a = b;
-        b = m;
+        b = r;
+        r = a % b;
     }
-    return m;
+    return b;
 }
 
 int lkkt(int a, int b) {
-    int lcd;
-    lcd = abs(a * b) / lnko(a, b);
+    int lcm;
+    lcm = abs(a * b) / lnko(a, b);
 }
 
 int power(int b, int e) {
@@ -32,7 +33,7 @@ void read_array(int *a, int n) {
 
 void print_array(int *a, int n) {
     for(int i = 0; i < n; ++i) {
-        cout << a[i];
+        cout << a[i] << ' ';
     }
 }
 
@@ -61,8 +62,8 @@ int binarySearch(const int *array, int size, int target, int left, int right) {
 }
 
 void selection_sort(int *a, int n) {
-    for(int i = 0 ; i < n; ++i) {
-        for(int j = i + 1; j < n - 1; ++j) {
+    for(int i = 0 ; i < n - 1; ++i) {
+        for(int j = i + 1; j < n; ++j) {
             if(a[i] > a[j]) {
                 swap(a[i], a[j]);
             }
@@ -72,9 +73,9 @@ void selection_sort(int *a, int n) {
 
 void minimselection_sort(int *a, int n) {
     int min_index;
-    for(int i = 0 ; i < n; ++i) {
+    for(int i = 0 ; i < n - 1; ++i) {
         min_index = i;
-        for(int j = i + 1; j < n - 1; ++j) {
+        for(int j = i + 1; j < n; ++j) {
             if(a[j] < a[min_index])
                 min_index = j;
         }
@@ -83,8 +84,74 @@ void minimselection_sort(int *a, int n) {
     }
 }
 
+void bubble_sort(int *a, int n) { // -2 -4 -5 7 5
+    int ok = 1;
+    while(ok == 1) {
+        for(int i = 0; i < n - 1 && ok == 1; ++i) {
+            if(a[i] > a[i + 1]) {
+                swap(a[i], a[i + 1]);
+            }
+        }
+        ok = 0;
+    }
+    swap(a[0], a[1]);
+}
 
+int lnkoRec(int a, int b) {
+    if(a % b == 0) return b;
+    else return lnkoRec(b, a % b);
+}
 
+int lkktRec(int a, int b, int a1, int b1) {
+    if(a1 % b1 == 0) return (a * b) / b1;
+    else return lkktRec(a, b, b1, a1 % b1);
+}
 
+int powerRec(int b, int e) {
+    if(e == 0) return 1;
+    return b * powerRec(b, e - 1);
+}
 
+void readArrayRec(int *a, int n, ifstream &f) {
+    if(n > 0) readArrayRec(a, n - 1, f);
+    f >> a[n - 1];
+}
 
+void printArrayRec(int *a, int n) {
+    if(n > 0) printArrayRec(a, n - 1);
+    cout << a[n - 1];
+}
+
+int sumArrayRec(int *a, int n) {
+    if(n == 1) return a[0];
+    return a[n - 1] + sumArrayRec(a, n - 1);
+}
+
+int maxArrayRec(int *a, int n, int max) {
+    int x;
+    if(n > 0) {
+        max = a[n - 1];
+        x = maxArrayRec(a, n - 1, max);
+        if(x > max) return x;
+        else return max;
+    }
+    else
+        return a[0];
+}
+
+int lnkoArrayRec(int *a, int n) {
+    if(n > 1)
+        return lnkoRec(a[n], lnkoArrayRec(a, n - 1));
+    else
+        return lnkoRec(a[0], a[1]);
+}
+
+int inverseNumber(int n, int fn) {
+    if(n < 10) {
+        return fn * 10 + n;
+    }
+    else {
+        fn = fn * 10 + n % 10;
+        return inverseNumber(n / 10, fn);
+    }
+}
--- lab-01/main.cpp
+++ lab-01/main.cpp
@@ -2,6 +2,155 @@
 #include "functions.h"
 
 int main() {
-
+    int feladatszam;
+    printf("Olvasd be a feladat szamat: ");
+    scanf("%i", &feladatszam);
+    switch (feladatszam) {
+        case 1: {
+            int x, y;
+            printf("Olvasd be a ket szamot: ");
+            scanf("%i%i", &x, &y);
+            printf("A ket szam legnagyobb kozos osztoja: %i", lnko(x, y));
+            break;
+        }
+        case 2: {
+            int x, y;
+            printf("Olvasd be a ket szamot: ");
+            scanf("%i%i", &x, &y);
+            printf("A ket szam legkisebb kozos tobbszorose: %i", lkkt(x, y));
+            break;
+        }
+        case 3: {
+            int x, y;
+            printf("Olvasd be a szamot es a hatvanyt: ");
+            scanf("%i%i", &x, &y);
+            printf("x ^ y = %i", power(x, y));
+            break;
+        }
+        case 4 /* es 5 */: {
+            int n;
+            printf("Olvasd be a tomb hosszat: ");
+            scanf("%i", &n);
+            int a[n];
+            read_array(a, n);
+            print_array(a, n);
+            break;
+        }
+        case 6: {
+            int n;
+            printf("Olvasd be a tomb hosszat: ");
+            scanf("%i", &n);
+            int a[n];
+            read_array(a, n);
+            int number;
+            printf("Olvasd be a keresett szamot: ");
+            scanf("%i", &number);
+            if(linearSearch(a, n, number) == 1)
+                printf("A keresett szam szerepel a tombben.");
+            else
+                printf("A keresett szam nem szerepel a tombben");
+            break;
+        }
+        case 8: {
+            int n;
+            printf("Olvasd be a tomb hosszat: ");
+            scanf("%i", &n);
+            int a[n];
+            read_array(a, n);
+            selection_sort(a, n);
+            printf("A tomb rendezes utan: \n");
+            print_array(a, n);
+            break;
+        }
+        case 9: {
+            int n;
+            printf("Olvasd be a tomb hosszat: ");
+            scanf("%i", &n);
+            int a[n];
+            read_array(a, n);
+            minimselection_sort(a, n);
+            printf("A tomb rendezes utan: \n");
+            print_array(a, n);
+            break;
+        }
+        case 10: {
+            int n;
+            printf("Olvasd be a tomb hosszat: ");
+            scanf("%i", &n);
+            int a[n];
+            read_array(a, n);
+            bubble_sort(a, n);
+            printf("A tomb rendezes utan: \n");
+            print_array(a, n);
+            break;
+        }
+        case 11: {
+            int x, y;
+            printf("Olvasd be a ket szamot: ");
+            scanf("%i%i", &x, &y);
+            printf("A ket szam legnagyobb kozos osztoja: %i", lnkoRec(x, y));
+            break;
+        }
+        case 12: {
+            int x, y, lcm;
+            printf("Olvasd be a ket szamot: ");
+            scanf("%i%i", &x, &y);
+            lcm = lkktRec(x, y, x,y);
+            printf("A ket szam legkisebb kozos tobbszorose: %i", lcm);
+            break;
+        }
+        case 13: {
+            int x, y;
+            printf("Olvasd be a szamot es a hatvanyt: ");
+            scanf("%i%i", &x, &y);
+            printf("x ^ y = %i", powerRec(x, y));
+            break;
+        }
+        case 14: /* es 15 */ {
+            int n;
+            printf("Olvasd be a tomb hosszat: ");
+            scanf("%i", &n);
+            int a[n];
+            //readArrayRec(a, n, f);
+            printArrayRec(a, n);
+            break;
+        }
+        case 16: {
+            int n;
+            printf("Olvasd be a tomb hosszat: ");
+            scanf("%i", &n);
+            int a[n];
+            read_array(a, n);
+            int sum = sumArrayRec(a, n);
+            printf("A tomb elemeinek osszege: %i", sum);
+            break;
+        }
+        case 17: {
+            int n;
+            printf("Olvasd be a tomb hosszat: ");
+            scanf("%i", &n);
+            int a[n];
+            read_array(a, n);
+            int max = maxArrayRec(a, n, a[n - 1]);
+            printf("A tomb maximuma: %i", max);
+            break;
+        }
+        case 18: {
+            int n;
+            printf("Olvasd be a tomb hosszat: ");
+            scanf("%i", &n);
+            int a[n];
+            read_array(a, n);
+            printf("A tomb elemeinek legnagyobb kozos osztoja: %i", lnkoArrayRec(a, n));
+            break;
+        }
+        case 20: {
+            int n, invn = 0;
+            printf("Olvasd be a szamot: ");
+            scanf("%i", &n);
+            invn = inverseNumber(n, invn);
+            printf("A szam forditottja: %i", invn);
+        }
+    }
     return 0;
 }
