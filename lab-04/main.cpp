d06c2e69407a477e44b6b1f67a997122d8e704f8
Gergely2004 <kis.tokai.gergely@student.ms.sapientia.ro>
2024-03-27 21:48:00 +0200

    n-Kiralyno

--- lab-04/main.cpp
+++ lab-04/main.cpp
@@ -6,27 +6,41 @@ void backTrackingDescartes(int *a, char *A, int *B, char C[][10], int n, int k, 
 void kiir1(int *a, int n);
 void kiir2(int *a, char *A, int *B, char C[][10], int n);
 void kiir3(int *a, int n);
+void kiirKiralyno(int *a, int n);
+
+void kiirExtra3(int *a, int n);
+
 bool igeretes1(int *a, int k);
 bool igeretes2(int *x, int k);
+bool igeretesKiralyno(int *x, int k);
+
+bool igeretesExtra3(int *x, int k, int s);
+
 void backTrackingPermutaciok(int *x, int n, int k, int &cnt);
 void backTrackingVariaciok(int *a, int n, int m, int k, int &cnt);
 void backTrackingKombinaciok(int *a, int n, int m, int k, int &cnt);
 void Reszhalmazok(int *a, int n, int k, int &cnt);
+void backTrackingKiralyno(int *a, int n, int k, int &cnt);
+
+void backExtra3(int *a, int n, int k, int s, int &cnt);
 
 
 int main() {
-    int n = 3, m = 2;
+    int n = 4, m = 2;
     int *a = new int[n];
     char A[3] = {'a', 'b', 'c'};
     int B[3] = {100, 300, 500};
     char C[3][10] = {"Csilla", "Peter", "Attila"};
     int cnt = 0;
+    int s = 500;  //10-es tobbszorosei
     //backTrackingKerekparzar(a, n, 0, cnt);
     //backTrackingDescartes(a, A, B, C, n, 0, cnt);
     //backTrackingPermutaciok(a, n, 0, cnt);
     //backTrackingVariaciok(a, n, m, 0, cnt);
     //backTrackingKombinaciok(a, n, m, 0, cnt);
-    Reszhalmazok(a, n, 0, cnt);
+    //Reszhalmazok(a, n, 0, cnt);
+    //backTrackingKiralyno(a, n, 1, cnt);
+    backExtra3(a, n, 1, s, cnt);
     cout << endl;
     cout << cnt;
     return 0;
@@ -110,6 +124,40 @@ void Reszhalmazok(int *a, int n, int k, int &cnt) {
     }
 }
 
+void backTrackingKiralyno(int *a, int n, int k, int &cnt) {
+    for (int j = 1; j <= n; ++j) {
+        a[k] = j;
+        if(igeretesKiralyno(a, k)) {
+            if(k == n) {
+                kiirKiralyno(a, n);
+                cnt++;
+            } else {
+                backTrackingKiralyno(a, n, k + 1, cnt);
+            }
+        }
+    }
+}
+
+void backExtra3(int *a, int n, int k, int s, int &cnt) {
+    for (int i = 1; i <= s / 10; ++i) {
+        a[k] = i * 10;
+        int sp = 0;
+        for (int j = 1; j <= k; ++j) {
+            sp += a[j];
+        }
+        if (igeretesExtra3(a, k, s)) {
+            if (k == n && sp == s) {
+                kiirExtra3(a, n);
+                cnt++;
+            } else {
+                if (k < n) {
+                    backExtra3(a, n, k + 1, s, cnt);
+                }
+            }
+        }
+    }
+}
+
 void kiir1(int *a, int n){
     for (int i = 0; i < n; ++i) {
         cout << a[i];
@@ -134,6 +182,28 @@ void kiir3(int *a, int n){
     cout << " ";
 }
 
+void kiirKiralyno(int *a, int n) {
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if(a[i] == j) {
+                cout << "Q ";
+            }
+            else {
+                cout << "- ";
+            }
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void kiirExtra3(int *a, int n) {
+    for (int i = 1; i <= n; ++i) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+}
+
 bool igeretes1(int *x, int k) {
     for(int i = 0; i <= k - 1; ++i) {
         if(x[i] == x[k])
@@ -146,6 +216,28 @@ bool igeretes2(int *x, int k) {
     for(int i = 0; i <= k - 1; ++i) {
         if(x[i] >= x[k])
             return false;
+    }
+    return true;
+}
+
+bool igeretesKiralyno(int *x, int k) {
+    for (int i = 1; i <= k - 1; ++i) {
+        if(abs(k - i) == abs(x[k] - x[i])) return false;
+        if(x[k] == x[i]) return false;
+    }
+    return true;
+}
+
+bool igeretesExtra3(int *x, int k, int s) {
+    if(x[k] >= x[k - 1]) {
+        return false;
+    }
+    int sp = 0;
+    for (int i = 1; i <= k; ++i) {
+        sp += x[i];
+    }
+    if(sp > s) {
+        return false;
     }
     return true;
 }
