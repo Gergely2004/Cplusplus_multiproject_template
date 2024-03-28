aa6cd147f8399b1a816a483d222bdcdd19f22671
Gergely2004 <kis.tokai.gergely@student.ms.sapientia.ro>
2024-03-28 13:48:54 +0200

    2024 03 28

--- lab-04/main.cpp
+++ lab-04/main.cpp
@@ -5,7 +5,8 @@ void backTrackingKerekparzar(int *a, int n, int k, int &cnt);
 void backTrackingDescartes(int *a, char *A, int *B, char C[][10], int n, int k, int &cnt);
 void kiir1(int *a, int n);
 void kiir2(int *a, char *A, int *B, char C[][10], int n);
-void kiir3(int *a, int n);
+void kiirSutik(int *a, int n, char **sutik);
+void printReszHalmaz(int *stack, int n, char** elemek);
 void kiirKiralyno(int *a, int n);
 
 void kiirExtra3(int *a, int n);
@@ -18,29 +19,30 @@ bool igeretesExtra3(int *x, int k, int s);
 
 void backTrackingPermutaciok(int *x, int n, int k, int &cnt);
 void backTrackingVariaciok(int *a, int n, int m, int k, int &cnt);
-void backTrackingKombinaciok(int *a, int n, int m, int k, int &cnt);
-void Reszhalmazok(int *a, int n, int k, int &cnt);
+void backTrackingKombinaciok(int *a, char** sutik, int n, int m, int k, int &cnt);
+void reszHalmaz(int *stack, char ** elemek, int n, int k);
 void backTrackingKiralyno(int *a, int n, int k, int &cnt);
 
 void backExtra3(int *a, int n, int k, int s, int &cnt);
 
-
 int main() {
-    int n = 4, m = 2;
+    int n = 5, m = 2;
     int *a = new int[n];
+    int elemek[] ={1,2, 3};
     char A[3] = {'a', 'b', 'c'};
     int B[3] = {100, 300, 500};
     char C[3][10] = {"Csilla", "Peter", "Attila"};
+    char *sutik[10] = {"perec", "pogacsa1", "pogacsa2", "kalacs", "sosrud"};
     int cnt = 0;
     int s = 500;  //10-es tobbszorosei
     //backTrackingKerekparzar(a, n, 0, cnt);
     //backTrackingDescartes(a, A, B, C, n, 0, cnt);
     //backTrackingPermutaciok(a, n, 0, cnt);
     //backTrackingVariaciok(a, n, m, 0, cnt);
-    //backTrackingKombinaciok(a, n, m, 0, cnt);
-    //Reszhalmazok(a, n, 0, cnt);
+    backTrackingKombinaciok(a, sutik, n, m, 0, cnt);
+    //reszHalmaz(a, sutik,n,0);
     //backTrackingKiralyno(a, n, 1, cnt);
-    backExtra3(a, n, 1, s, cnt);
+    //backExtra3(a, n, 1, s, cnt);
     cout << endl;
     cout << cnt;
     return 0;
@@ -53,7 +55,7 @@ void backTrackingKerekparzar(int *a, int n, int k, int &cnt) {
         }
         else {
             cnt++;
-            kiir1(a, n);
+            //kiir1(a, n);
         }
     }
 }
@@ -98,30 +100,32 @@ void backTrackingVariaciok(int *a, int n, int m, int k, int &cnt) {
     }
 }
 
-void backTrackingKombinaciok(int *a, int n, int m, int k, int &cnt) {
+void backTrackingKombinaciok(int *a, char **sutik, int n, int m, int k, int &cnt) {
     for (a[k] = 1; a[k] <= n; ++a[k]) {
         if(igeretes2(a, k)) {
             if (k < m - 1) {
-                backTrackingKombinaciok(a, n, m, k + 1, cnt);
+                backTrackingKombinaciok(a, sutik, n, m, k + 1, cnt);
             }
             else {
                 cnt++;
-                kiir1(a, m);
+                kiirSutik(a, m, sutik);
             }
         }
     }
 }
 
-void Reszhalmazok(int *a, int n, int k, int &cnt) {
-    for (a[k] = 0; a[k] <= 1; ++a[k]) {
-        if (k < n - 1) {
-            Reszhalmazok(a, n, k + 1, cnt);
+void reszHalmaz(int *stack, char** elemek, int n, int k){
+    for(int i = 0; i <= 1; i++) {
+        if(k<n) {
+            stack[k] = i;
+            reszHalmaz(stack, elemek, n, k + 1);
         }
         else {
-            cnt++;
-            kiir3(a, n);
+            printReszHalmaz(stack, k, elemek);
+            return;
         }
     }
+
 }
 
 void backTrackingKiralyno(int *a, int n, int k, int &cnt) {
@@ -160,9 +164,9 @@ void backExtra3(int *a, int n, int k, int s, int &cnt) {
 
 void kiir1(int *a, int n){
     for (int i = 0; i < n; ++i) {
-        cout << a[i];
+        cout << a[i] << " ";
     }
-    cout << " ";
+    cout << endl;
 }
 
 void kiir2(int *a, char *A, int *B, char C[][10], int n) {
@@ -174,12 +178,31 @@ void kiir2(int *a, char *A, int *B, char C[][10], int n) {
     cout << " ";
 }
 
-void kiir3(int *a, int n){
-    for (int i = 0; i < n; ++i) {
-        if(a[i] == 1)
-            cout << i + 1;
+void printReszHalmaz(int *stack, int n, char** elemek) {
+    for(int i = 0; i <n; i++) {
+        if(i == 0){
+            cout<<"{";
+        }
+        if(stack[i] == 1) {
+            if(i == n-1){
+                cout << elemek[i];
+            }
+            else{
+                cout << elemek[i] << ",";
+            }
+        }
+        if(i == n-1){
+            cout<<"}";
+        }
     }
-    cout << " ";
+    cout << endl;
+}
+
+void kiirSutik(int *a, int n, char **sutik) {
+    for (int i = 0; i < n; ++i) {
+        cout << sutik[a[i] - 1] << " ";
+    }
+    cout << endl;
 }
 
 void kiirKiralyno(int *a, int n) {
@@ -189,7 +212,7 @@ void kiirKiralyno(int *a, int n) {
                 cout << "Q ";
             }
             else {
-                cout << "- ";
+                cout << "~ ";
             }
         }
         cout << endl;
