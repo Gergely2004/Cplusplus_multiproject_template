d979020b7014dd14932ae8fc164db24a41bab528
Gergely2004 <kis.tokai.gergely@student.ms.sapientia.ro>
2024-05-23 08:23:58 +0300

    lab12 hazi

--- lab-12/main.cpp
+++ lab-12/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <stack>
+
 using namespace std;
 
 ifstream fin("be.txt");
+ifstream fin1("hazi.txt");
 
 int n, m;
 int adjMat[100][100];
@@ -21,36 +24,74 @@ void dfs(int u) {
     color[u] = 2;
 }
 
+int pathlength[15]={0};
+void dfsX(int u) {
+    color[u] = 1;
+    cout << u << " ";
+    for (int i = 0; i < adjList[u].size(); ++i) {
+        int v = adjList[u][i];
+        if (color[v] == 0) {
+            pathlength[v] = pathlength[u]+1;
+            dfsX(v);
+        }
+    }
+    color[u] =2;
+}
+
+
 int main() {
-    int x, y, c;
-    fin >> n >> m;
-    for (int i = 0; i < m; ++i) {
-        fin >> x >> y >> c;
-        adjMat[x][y] = adjMat[y][x] = c;
-        adjList[x].push_back(y);
-        adjList[y].push_back(x);
-    }
-    
-    for (int i = 1; i <= n; ++i) {
-        cout << i << ": ";
-        for (auto it: adjList[i]) {
-            cout << it << " -> ";
+    int feladatszam;
+    cout << "ORAI ANYAG / HAZI FELADAT [1 / 2]" << endl;
+    cin >> feladatszam;
+    switch (feladatszam) {
+        case 1: {
+            int x, y, c;
+            fin >> n >> m;
+            for (int i = 0; i < m; ++i) {
+                fin >> x >> y >> c;
+                adjMat[x][y] = adjMat[y][x] = c;
+                adjList[x].push_back(y);
+                adjList[y].push_back(x);
+            }
+
+            for (int i = 1; i <= n; ++i) {
+                cout << i << ": ";
+                for (auto it: adjList[i]) {
+                    cout << it << " -> ";
+                }
+                cout << endl;
+            }
+
+            cout << endl;
+
+            for (int i = 1; i <= n; ++i) {
+                for (int j = 1; j <= n; ++j) {
+                    cout << adjMat[i][j] << " ";
+                }
+                cout << endl;
+            }
+
+            cout << endl;
+            dfs(1);
         }
-        cout << endl;
-    }
-
-    cout << endl;
-
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            cout << adjMat[i][j] << " ";
+        case 2: {
+            int x, y, c;
+            fin1 >> n >> m;
+            int X;  //a megadott informacio a nagyfonokhoz valo eljutasi ideje - X ora
+            cout << "Add meg az orak szamat: ";
+            cin >> X;
+            stack <int> track;
+            cout << "DFS Traversal: " << endl;
+            dfsX(1);
+            cout << endl;
+            cout << "Alkalmazottak akiknek eler a jelentese: " << endl;
+            for (int i = 0; i < n; ++i) {
+                if(pathlength[i] <= X && color[i] != 0){
+                    cout << i << " ";
+                }
+            }
+            cout<<endl;
         }
-        cout << endl;
     }
-
-    cout << endl;
-
-    dfs(1);
-
     return 0;
 }
